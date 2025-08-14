b8bb041450fe35e18c1384499f56fa7a91e0ed73
Bharath Katabattula <bharath632@revature.com>
2025-08-14 14:12:40 +0530

    Integrate OpenAI model

---
+++ agents/__init__.py
@@ -0,0 +1,3 @@
+from .central_orchestrator_agent import root_agent
+
+__all__ = ["root_agent"]
--- agents/central_orchestrator_agent.py
+++ agents/central_orchestrator_agent.py
@@ -1,10 +1,14 @@
 from google.adk.agents import LlmAgent
+from google.adk.models.lite_llm import LiteLlm
+from utils.config import LLM_MODEL
 
 central_orchestrator_agent = LlmAgent(
     name="central_orchestrator_agent",
     description=" Central Orchestrator Agent",
-    instruction="",
-    model="LLM_MODEL",
+    instruction="Greet the user",
+    model=LiteLlm(model=LLM_MODEL),
     tools=[],
     output_key="central_orchestrator_agent_result"
 )
+
+root_agent = central_orchestrator_agent
